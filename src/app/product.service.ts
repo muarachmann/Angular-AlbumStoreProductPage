44d2c5bde45aee21e21ece3a1eee545fed3f7601
muarachmann <muarachmann@gmail.com>
2019-10-25 21:50:45 +0100

    Added product service

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+}
