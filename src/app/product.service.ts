09acb6500d0e66051dca5183cc150b3e66d6e122
muarachmann <muarachmann@gmail.com>
2019-10-25 22:47:37 +0100

    returend album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,7 +12,8 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return (this._http.get(this._albumUrl)).map(response => <Album> response.json());
+    return (this._http.get(this._albumUrl)).map(response =>
+        <Album>response.json());
   }
 
 }
